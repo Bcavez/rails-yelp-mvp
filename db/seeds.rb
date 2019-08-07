d4cfe7ce3204a5574aa008f2681481f0be84022e
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-07 14:33:00 +0200

    added routes, controllers, views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Restaurant.create(name: Faker::Movies::HarryPotter.character, address: Faker::Movies::HarryPotter.location, phone_number: Faker::Movies::HarryPotter.spell, category: "chinese")
+end
